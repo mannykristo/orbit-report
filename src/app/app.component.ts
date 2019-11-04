21a2bfc99c557133f7fc4339018eaba83697f8d1
Manuel Kristo <manny.kristo@gmail.com>
2019-11-03 22:04:31 -0600

    finally step 8 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,41 @@ import { Satellite } from './satellite';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'orbit-report';
 
+export class AppComponent {
+  title = 'Orbit Report';
+
+  displayList: Satellite[];
   sourceList: Satellite[];
   Url: string = 'https://handlers.education.launchcode.org/static/satellites.json';
 
   constructor() {
-   this.sourceList = [];
-   window.fetch(this.Url).then(function(response) {
+    this.sourceList = [];
+    this.displayList = [];
+
+    window.fetch(this.Url).then(function(response) {
       response.json().then(function(data) {
         let satellites = data.satellites;
         for(let i=0; i < satellites.length; i++) {
           let satellite = new Satellite(satellites[i].name, satellites[i].type, satellites[i].launchDate, satellites[i].orbitType, satellites[i].operational);
           this.sourceList.push(satellite);
         }
+        this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
+
   }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLocaleLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLocaleLowerCase();
+      if (name.includes(searchTerm)) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    this.displayList = matchingSatellites;
+  }
+
 }
